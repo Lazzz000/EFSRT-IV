558f64af03d2ee4528229358f77e6e2bc2863b67
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-02-13 18:13:26 -0500

    feat: inyectar ProductoService en el controlador

--- src/main/java/com/minimarket/app/controlador/AdminController.java
+++ src/main/java/com/minimarket/app/controlador/AdminController.java
@@ -1,13 +1,18 @@
 package com.minimarket.app.controlador;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.minimarket.app.servicio.ProductoServicio;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	private ProductoServicio productoServicio;
 
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
@@ -17,6 +22,7 @@ public class AdminController {
 
     @GetMapping("/productos")
     public String productos(Model model) {
+    
         model.addAttribute("content", "admin/productos");
         return "layout/admin-layout";
     }
