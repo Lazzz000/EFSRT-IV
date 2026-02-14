5d35fd260dd18b3630f1f41d95d5dffd470a7ee0
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-02-14 01:10:38 -0500

    feat: agregar campos(marca,categ,dscrp) al tbody,modal y a las fun js

--- src/main/java/com/minimarket/app/controlador/AdminController.java
+++ src/main/java/com/minimarket/app/controlador/AdminController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.minimarket.app.servicio.CategoriaServicio;
+import com.minimarket.app.servicio.MarcaServicio;
 import com.minimarket.app.servicio.ProductoServicio;
 
 @Controller
@@ -22,9 +24,19 @@ public class AdminController {
         return "layout/admin-layout";
     }
 
+	@Autowired
+	private CategoriaServicio categoriaServicio; 
+
+	@Autowired
+	private MarcaServicio marcaServicio;
+	
     @GetMapping("/productos")
     public String productos(Model model) {
     	model.addAttribute("productos", productoServicio.listarTodos());
+    	//enviamos las listas para los select del modal
+    	model.addAttribute("categorias", categoriaServicio.listarTodas());
+    	model.addAttribute("marcas", marcaServicio.listarTodos());
+  
         model.addAttribute("content", "admin/productos");
         return "layout/admin-layout";
     }
